7c648bee966c4f821038354a0df7c1d5ebc4b413
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2023-12-06 13:57:20 +0300

    add select and increment level

--- res/js/logic.js
+++ res/js/logic.js
@@ -14,6 +14,62 @@ function getRandomBgrImage(last) {
     return "qrc:/res/images/backgrounds/bgr" + bgrStr + ".jpg"
 }
 
+function fillModelFromLevel(m_srcModel,m_dstModel,m_level){    
+    addEmptyCell(m_dstModel)
+    for( var i = 0; i < m_srcModel.rowCount(); i++ ) {
+        if ( m_srcModel.get(i).lvl === m_level){
+            m_dstModel.setProperty(i, "cell", m_srcModel.get(i).cell)
+        }
+    }  
+}
 
+function clickOnTile( m_model, x_pos,y_pos ){
 
+    let m_index = -1
+    let m_value
 
+    if ( (x_pos - 1) >= 0 ){
+        m_index = (x_pos - 1) + (y_pos *5)
+        m_value = (m_model.get(m_index).cell) ? 0 : 1
+        m_model.setProperty(m_index, "cell", m_value)
+    }
+
+    if ( (x_pos + 1) < 5 ){
+        m_index = (x_pos + 1) + (y_pos *5)
+        m_value = (m_model.get(m_index).cell) ? 0 : 1
+        m_model.setProperty(m_index, "cell", m_value)
+    }
+
+    if ( (y_pos - 1) >= 0 ){
+        m_index = x_pos + ( (y_pos - 1) *5)
+        m_value = (m_model.get(m_index).cell) ? 0 : 1
+        m_model.setProperty(m_index, "cell", m_value)
+    }
+
+    if ( (y_pos + 1) < 5 ){
+        m_index = x_pos + ( (y_pos + 1) *5)
+        m_value = (m_model.get(m_index).cell) ? 0 : 1
+        m_model.setProperty(m_index, "cell", m_value)
+    }
+}
+
+function clearAll( m_model ){
+    for( var i = 0; i < m_model.rowCount(); i++ ) {
+        m_model.setProperty(i, "cell", 0)
+    }
+}
+function addEmptyCell( m_model ){
+    m_model.clear()
+    for( var i = 0; i < 25; i++ ) {
+        m_model.append({"cell":0})
+    }
+}
+function isWinGame( m_model ){
+    let flag= false
+
+    for( var i = 0; i < m_model.rowCount(); i++ ) {
+        flag = flag || m_model.get(i).cell ;
+    }
+
+    return !flag
+}
